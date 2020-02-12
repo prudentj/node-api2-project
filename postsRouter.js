1fcb7055c5c36c104efb00087aae75782b24d463
Jacob Prudent <jake.prudent@gmail.com>
2020-02-11 23:09:33 -0800

    MVP

--- postsRouter.js
+++ postsRouter.js
@@ -6,14 +6,16 @@ const postDB = require('./data/db');
 
 //Post
 
-// /api/posts              Creates a post using the information sent inside the `request body`.
-
+// /api/posts Creates a post using the information sent inside the `request body`.
+//This one isn't working and I don't know why
 router.post('/', (req, res) => {
 	const postData = req.body;
 	if (!postData.title || !postData.contents) {
 		res
 			.status(400)
 			.json({errorMessage: 'Please provide title and contents for the post.'});
+		console.log(postData.title);
+		console.log(postData.contents);
 	} else {
 		postDB
 			.insert(postData)
@@ -71,22 +73,6 @@ router.get('/', (req, res) => {
 				.json({error: 'The post information could not be retrieved.'});
 		});
 });
-// router.get('/', (req, res) => {
-// 	const pagination = req.query;
-
-// 	// console.log('pagination', pagination);
-// 	postDB
-// 		.find(pag)
-// 		.then(posts => {
-// 			res.status(200).json(posts);
-// 		})
-// 		.catch(err => {
-// 			console.log('error 500', err);
-// 			res
-// 				.status(500)
-// 				.json({error: 'The posts information could not be retrieved.'});
-// 		});
-// });
 
 // /api/posts/:id      Returns the post object with the specified id.
 router.get('/:id', (req, res) => {
@@ -158,7 +144,8 @@ router.delete('/:id', (req, res) => {
 //Put
 // /api/posts/:id  Updates post with specified `id` using data from the `request body`. Returns the modified document, **NOT the original**.                                           |
 router.put('/:id', (req, res) => {
-	Posts.update(req.params.id, req.body)
+	postDB
+		.update(req.params.id, req.body)
 		.then(posts => {
 			if (posts === undefined) {
 				res
@@ -181,131 +168,3 @@ router.put('/:id', (req, res) => {
 });
 
 module.exports = router;
-// Configure the API to handle to the following routes:
-
-//example from lesson
-
-// // route handlers - handles what comes after /api/hubs
-// router.get("/", (req, res) => {
-//     const pagination = req.query;
-
-//     console.log("pagination", pagination);
-
-//     Hubs.find(pagination)
-//       .then(hubs => {
-//         res.status(200).json(hubs);
-//       })
-//       .catch(error => {
-//         // log error to database
-//         console.log(error);
-//         res.status(500).json({
-//           message: "Error retrieving the hubs",
-//         });
-//       });
-//   });
-
-//   router.get("/:id", (req, res) => {
-//     Hubs.findById(req.params.id)
-//       .then(hub => {
-//         if (hub) {
-//           res.status(200).json(hub);
-//         } else {
-//           res.status(404).json({ message: "Hub not found" });
-//         }
-//       })
-//       .catch(error => {
-//         // log error to database
-//         console.log(error);
-//         res.status(500).json({
-//           message: "Error retrieving the hub",
-//         });
-//       });
-//   });
-
-//   router.post("/", (req, res) => {
-//     Hubs.add(req.body)
-//       .then(hub => {
-//         res.status(201).json(hub);
-//       })
-//       .catch(error => {
-//         // log error to database
-//         console.log(error);
-//         res.status(500).json({
-//           message: "Error adding the hub",
-//         });
-//       });
-//   });
-
-//   router.delete("/:id", (req, res) => {
-//     Hubs.remove(req.params.id)
-//       .then(count => {
-//         if (count > 0) {
-//           res.status(200).json({ message: "The hub has been nuked" });
-//         } else {
-//           res.status(404).json({ message: "The hub could not be found" });
-//         }
-//       })
-//       .catch(error => {
-//         // log error to database
-//         console.log(error);
-//         res.status(500).json({
-//           message: "Error removing the hub",
-//         });
-//       });
-//   });
-
-//   router.put("/:id", (req, res) => {
-//     const changes = req.body;
-//     Hubs.update(req.params.id, changes)
-//       .then(hub => {
-//         if (hub) {
-//           res.status(200).json(hub);
-//         } else {
-//           res.status(404).json({ message: "The hub could not be found" });
-//         }
-//       })
-//       .catch(error => {
-//         // log error to database
-//         console.log(error);
-//         res.status(500).json({
-//           message: "Error updating the hub",
-//         });
-//       });
-//   });
-
-//   // add an endpoint that returns all the messages for a hub
-//   router.get("/:id/messages", (req, res) => {
-//     const { id } = req.params;
-
-//     Hubs.findHubMessages(id)
-//       .then(messages => {
-//         res.status(200).json(messages);
-//       })
-//       .catch(error => {
-//         console.log(error);
-
-//         res.status(500).json({ errorMessage: "sorry, we failed you" });
-//       });
-//   });
-
-//   // add an endpoint for adding new message to a hub
-//   router.post("/:id/messages", (req, res) => {
-//     const { id } = req.params;
-
-//     const message = { ...req.body, hub_id: id };
-
-//     Hubs.addMessage(message)
-//       .then(inserted => {
-//         res.status(201).json(inserted);
-//       })
-//       .catch(error => {
-//         console.log(error);
-
-//         res.status(500).json({ errorMessage: "sorry, we failed you" });
-//       });
-//   });
-
-//   // mind the S in exportS
-//   module.exports = router; // same as below
-
-//   // export default router; // ES2015 Modules
